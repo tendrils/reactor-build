d018c59b8d4e63e2ba5e94dd0825ba2cd0e653ed
tendrils <tendrils@users.noreply.github.com>
2019-08-30 12:31:10 +1200

    first pass at C project support

--- module/c_binary/module.mk
+++ module/c_binary/module.mk
@@ -9,57 +9,130 @@ C_BINARY_PROJECT_TYPE =
 define f_c_binary_init =
 	$(call f_define_build_action,compile_c,f_c_binary_do_compile_c)
 	$(call f_define_build_action,compile_cxx,f_c_binary_do_compile_cxx)
+	$(call f_define_build_action,compile_c_to_deps,f_c_binary_do_compile_c_to_deps)
+	$(call f_define_build_action,compile_cxx_to_deps,f_c_binary_do_compile_cxx_to_deps)
 	$(call f_define_build_action,compile_c_to_asm,f_c_binary_do_compile_c_to_asm)
 	$(call f_define_build_action,compile_cxx_to_asm,f_c_binary_do_compile_cxx_to_asm)
 	$(call f_define_build_action,compile_asm,f_c_binary_do_compile_asm)
+	$(call f_define_build_action,link_binary,f_c_binary_do_link_binary)
+
+	$(call f_define_build_action,compile_c_with_tc,f_c_binary_do_compile_c_with_tc)
+	$(call f_define_build_action,compile_cxx_with_tc,f_c_binary_do_compile_cxx_with_tc)
+	$(call f_define_build_action,compile_c_to_deps,f_c_binary_do_compile_c_to_deps_with_tc)
+	$(call f_define_build_action,compile_cxx_to_deps,f_c_binary_do_compile_cxx_to_deps_with_tc)
+	$(call f_define_build_action,compile_c_to_asm_with_tc,f_c_binary_do_compile_c_to_asm_with_tc)
+	$(call f_define_build_action,compile_cxx_to_asm_with_tc,f_c_binary_do_compile_cxx_to_asm_with_tc)
+	$(call f_define_build_action,compile_asm_with_tc,f_c_binary_do_compile_asm_with_tc)
+	$(call f_define_build_action,link_binary_with_tc,f_c_binary_do_link_binary_with_tc)
 endef
 
 define f_c_binary_toolchain_define =
-	$(call f_util_override_append_if_absent,C_BINARY_TOOLCHAINS,$1)
-	$(call f_util_set_symbol,c_binary_tc_type_$1,$2)
-	$(call f_util_set_symbol,c_binary_tc_arch_$1,$3)
-	$(call f_util_set_symbol,c_binary_tc_os_$1,$4)
-	$(call f_util_set_symbol,c_binary_tc_abi_$1,$5)
+	$(call f_util_override_append_if_absent,v_c_binary_toolchains,$1)
+	$(call f_util_set_symbol,v_c_binary_tc_type_$1,$2)
+	$(call f_util_set_symbol,v_c_binary_tc_arch_$1,$3)
+	$(call f_util_set_symbol,v_c_binary_tc_os_$1,$4)
+	$(call f_util_set_symbol,v_c_binary_tc_abi_$1,$5)
 	$(call f_util_log_debug,c_binary,c/c++ toolchain provider registered: $1)
-	$(if $(C_BINARY_TOOLCHAIN_DEFAULT),,\
-		$(call f_c_binary_tc_default_set,$1))
+	$(if $(v_c_binary_toolchain_default),,$(call f_c_binary_tc_default_set,$1))
+endef
+
+define f_c_binary_project_type_define =
+	$(call f_util_override_append_if_absent,v_c_binary_project_types,$1)
 endef
 
 define f_c_binary_tc_default_set =
-	$(call f_util_override_set_symbol,C_BINARY_TC_DEFAULT,$1)
+	$(call f_util_override_set_symbol,v_c_binary_toolchain_default,$1)
 	$(call f_util_log_debug,c_binary,default c/c++ toolchain provider set to [$1])
 endef
 
-define f_c_binary_tc_cmd_set =
-	$(call f_util_override_set_symbol,fp_c_binary_tc_$1_cmd_$2,$3)
+# f_c_binary_cmd_invoke(command, in_files, out_file)
+#
+# -> generic command handler function
+# -> pass command to active toolchain, or default if none is selected
+#
+# command ($1): the name of the toolchain command to invoke
+# in_files ($2): the name of the input file(s)
+# out_file ($3): the name of the output file
+#
+define f_c_binary_cmd_invoke =
+	$(if $(v_c_binary_toolchain_default),,\
+		$(call f_util_fatal_error,c-binary,no default c/c++ toolchain provider set))
+	$(if $(v_c_binary_toolchain_active),\
+		$(call f_c_binary_tc_cmd_invoke,$(v_c_binary_toolchain_active),$1,$2,$3),\
+    	$(call f_c_binary_tc_cmd_invoke,$(v_c_binary_toolchain_default),$1,$2,$3))
 endef
 
+define f_c_binary_tc_active_set =
+	$(if $1,\
+		$(call f_util_log_debug,c_binary,activated c/c++ toolchain [$(v_c_binary_toolchain_active)])\
+		$(call f_util_set_symbol,v_c_binary_toolchain_active,$1),\
+		$(call f_util_log_debug,c_binary,deactivated c/c++ toolchain [$(v_c_binary_toolchain_active)])\
+		$(call f_util_unset_symbol,v_c_binary_toolchain_active,$1))
+endef
+
+define f_c_binary_tc_active_get =
+	$(if $(v_c_binary_toolchain_active),\
+		$(v_c_binary_toolchain_active),\
+		$(v_c_binary_toolchain_default))
+endef
+
+define f_c_binary_tc_cmd_set =
+	$(call f_util_override_set_symbol,fp_c_binary_tc_$1_cmd_$2,$(call $3,$1))
+endef
+
+# f_c_binary_tc_cmd_invoke(command, in_files, out_file)
+#
+# -> specific command handler function
+# -> pass command to specified toolchain
+# -> called by generic handler function f_c_binary_cmd_invoke
+#
+# toolchain ($1): the id of the target toolchain
+# command ($2): the name of the toolchain command to invoke
+# in_files ($3): the name of the input file(s)
+# out_file ($4): the name of the output file
+#
 define f_c_binary_tc_cmd_invoke =
 	$(if $(fp_c_binary_tc_$1_cmd_$2),,\
 		$(call f_util_fatal_error,c-binary,no [$2] command set for c/c++ toolchain provider [$(C_BINARY_TOOLCHAIN_DEFAULT)]))
 	$(call $(fp_c_binary_tc_$1_cmd_$2),$3,$4)
 endef
 
-define f_c_binary_cmd_invoke =
-	$(if $(C_BINARY_TOOLCHAIN_DEFAULT),,\
-		$(call f_util_fatal_error,c-binary,no default c/c++ toolchain provider set))
-    $(call f_c_binary_tc_cmd_invoke,default,$1,$2,$3)
-endef
-
-fp_c_binary_tc_default_cmd_compile_c = $(ff_c_binary_tc_$(C_BINARY_TOOLCHAIN_DEFAULT)_cmd_compile_c)
-fp_c_binary_tc_default_cmd_compile_cxx = $(ff_c_binary_tc_$(C_BINARY_TOOLCHAIN_DEFAULT)_cmd_compile_cxx)
-fp_c_binary_tc_default_cmd_compile_c_to_asm = $(ff_c_binary_tc_$(C_BINARY_TOOLCHAIN_DEFAULT)_cmd_compile_c_to_asm)
-fp_c_binary_tc_default_cmd_compile_cxx_to_asm = $(ff_c_binary_tc_$(C_BINARY_TOOLCHAIN_DEFAULT)_cmd_compile_c)
-
+# generic command dispatch functions
 f_c_binary_do_compile_c = $(call f_c_binary_cmd_invoke,compile_c,$1,$2)
 f_c_binary_do_compile_cxx = $(call f_c_binary_cmd_invoke,compile_cxx,$1,$2)
 f_c_binary_do_compile_c_to_asm = $(call f_c_binary_cmd_invoke,compile_c_to_asm,$1,$2)
 f_c_binary_do_compile_cxx_to_asm = $(call f_c_binary_cmd_invoke,compile_cxx_to_asm,$1,$2)
+f_c_binary_do_link_binary = $(call f_c_binary_cmd_invoke,link_binary,$1,$2)
 
-$(C_SRC_DIR)/%.o: %.c
+# selective command dispatch functions
+f_c_binary_do_compile_c_with_tc = $(call f_c_binary_tc_cmd_invoke,compile_c_with_tc,$1,$2)
+f_c_binary_do_compile_cxx_with_tc = $(call f_c_binary_tc_cmd_invoke,compile_cxx_with_tc,$1,$2)
+f_c_binary_do_compile_c_to_asm_with_tc = $(call f_c_binary_tc_cmd_invoke,compile_c_to_asm_with_tc,$1,$2)
+f_c_binary_do_compile_cxx_to_asm_with_tc = $(call f_c_binary_tc_cmd_invoke,compile_cxx_to_asm_with_tc,$1,$2)
+f_c_binary_do_link_binary_with_tc = $(call f_c_binary_tc_cmd_invoke,link_binary_with_tc,$1,$2)
+
+%.c.o: %.c
 	$(call f_action_compile_c,$^,$@)
 
-$(CXX_SRC_DIR)/%.o: %.c
+%.c.s: %.c
+	$(call f_action_compile_c_to_asm,$^,$@)
+
+%.c.d: %.c
+	$(call f_action_compile_c_to_deps,$^,$@)
+
+%.cpp.o: %.cpp
 	$(call f_action_compile_cxx,$^,$@)
+
+%.cpp.s: %.cpp
+	$(call f_action_compile_cxx_to_asm,$^,$@)
+
+%.cpp.d: %.cpp
+	$(call f_action_compile_cpp_to_deps,$^,$@)
+
+%.s.o: %.s
+	$(call f_action_compile_asm,$^,$@)
+
+%.S.o: %.S
+	$(call f_action_compile_asm,$^,$@)
 
 endif
--- module/gcc/module.mk
+++ module/gcc/module.mk
@@ -11,29 +11,64 @@ define f_gcc_init =
 
 endef
 
+# shell command templates for GCC toolchain commands
+fm_gcc_tc_cmd_compile_c=$$(v_gcc_tc_cross_prefix_$1)gcc $$(v_gcc_cflags_$1) $$1 -o $$2
+fm_gcc_tc_cmd_compile_cxx=$$(v_gcc_tc_cross_prefix_$1)g++ $$(v_gcc_tc_cflags_$1) $$1 -o $$2
+fm_gcc_tc_cmd_compile_c_to_deps=$$(v_gcc_tc_cross_prefix_$1)gcc -M $$(v_gcc_tc_cflags_$1) $$1 -o $$2
+fm_gcc_tc_cmd_compile_cxx_to_deps=$$(v_gcc_tc_cross_prefix_$1)g++ -M $$(v_gcc_tc_cflags_$1) $$1 -o $$2
+fm_gcc_tc_cmd_compile_c_to_asm=$$(v_gcc_tc_cross_prefix_$1)gcc -S $$(v_gcc_tc_cflags_$1) $$1 -o $$2
+fm_gcc_tc_cmd_compile_cxx_to_asm=$$(v_gcc_tc_cross_prefix_$1)g++ -S $$(v_gcc_tc_cflags_$1) $$1 -o $$2
+fm_gcc_tc_cmd_compile_asm=$$(v_gcc_tc_cross_prefix_$1)as $$(v_gcc_tc_cflags_$1) $$1 -o $$2
+fm_gcc_tc_cmd_link_binary=$$(v_gcc_tc_cross_prefix_$1)ld -o $$1 $$2 $$(v_gcc_tc_ldflags_$1)
+fm_gcc_tc_cmd_strip_binary=$$(v_gcc_tc_cross_prefix_$1)objcopy -I $$3 -O $$4 $$1 $$2
+fm_gcc_tc_cmd_archive_static=$$(v_gcc_tc_cross_prefix_$1)ar qc $$(v_gcc_tc_flag_lto_$1) $$1 $$2
+fm_gcc_tc_cmd_update_static=$$(v_gcc_tc_cross_prefix_$1)ranlib $$(v_gcc_tc_flag_lto_$1) $$1
+
+# internal command for ReBuild to locate LTO plugin for the given GCC toolchain
+f_gcc_tc_cmd_find_lto_plugin=$(v_gcc_tc_cross_prefix_$1)gcc --print-file-name=liblto_plugin.so
+
+# f_gcc_toolchain_define:
+#   register a new GCC-based toolchain and its associated commands
 # ($1): handle for new toolchain
 # ($2): [optional] cross-compilation target
 define f_gcc_toolchain_define =
     $(call f_c_binary_toolchain_define,$1)
 	$(call f_util_override_append_if_absent,GCC_TOOLCHAINS,$1)
     $(if $2,$(call f_gcc_cross_target_set,$1,$2),)
-    $(call f_c_binary_tc_cmd_set,$1,)
+    $(call f_gcc_tc_flag_lto_set,$1)
+    $(call f_c_binary_tc_cmd_set,$1,compile_c,fm_gcc_tc_cmd_compile_c)
+    $(call f_c_binary_tc_cmd_set,$1,compile_cxx,fm_gcc_tc_cmd_compile_cxx)
+    $(call f_c_binary_tc_cmd_set,$1,compile_c_to_deps,fm_gcc_tc_cmd_compile_c_to_deps)
+    $(call f_c_binary_tc_cmd_set,$1,compile_cxx_to_deps,fm_gcc_tc_cmd_compile_cxx_to_deps)
+    $(call f_c_binary_tc_cmd_set,$1,compile_c_to_asm,fm_gcc_tc_cmd_compile_c_to_asm)
+    $(call f_c_binary_tc_cmd_set,$1,compile_cxx_to_asm,fm_gcc_tc_cmd_compile_cxx_to_asm)
+    $(call f_c_binary_tc_cmd_set,$1,compile_asm,fm_gcc_tc_cmd_compile_asm)
+    $(call f_c_binary_tc_cmd_set,$1,link_binary,fm_gcc_tc_cmd_link_binary)
+    $(call f_c_binary_tc_cmd_set,$1,strip_binary,fm_gcc_tc_cmd_strip_binary)
+    $(call f_c_binary_tc_cmd_set,$1,create_static_lib,fm_gcc_tc_cmd_create_static_lib)
+    $(call f_c_binary_tc_cmd_set,$1,update_static_lib,fm_gcc_tc_cmd_update_static_lib)
+endef
+
+define f_gcc_tc_flag_lto_set =
+    $(call f_util_set_symbol,v_gcc_tc_flag_lto_$1,--plugin=$(shell $(call f_gcc_tc_cmd_find_lto_plugin,$1)))
+    $(call f_util_log_trace,gcc,LTO flag for toolchain $1 set to [$(v_gcc_tc_flag_lto_$1)])
 endef
 
 define f_gcc_tc_cross_target_set =
-    $(call f_util_set_symbol,gcc_cross_target_$1,$2)
-    $(if $2,$(call f_util_override_set_symbol,v_gcc_tc_cmd_$,$(GCC_CROSS_TARGET)-),)
+    $(call f_util_set_symbol,v_gcc_tc_cross_target_$1,$2)
+    $(if $2,\
+        $(call f_util_override_set_symbol,\
+            v_gcc_tc_cross_prefix_$1,$(v_gcc_tc_cross_target_$1)-),\
+        $(call f_util_override_unset_symbol,v_gcc_tc_cross_prefix_$1))
 endef
 
-CC=$(XC_PREFIX)gcc
-CXX=$(XC_PREFIX)g++
-AS=$(XC_PREFIX)as
-LD=$(XC_PREFIX)gcc
-OBJCOPY=$(XC_PREFIX)objcopy
+define f_gcc_tc_cflags_set =
+    $(call f_util_set_symbol,v_gcc_tc_cflags_$1,$2)
+endef
 
-LTOFLAG:=--plugin=$(shell $(CC) --print-file-name=liblto_plugin.so)
-AR=$(XC_PREFIX)ar qc $(LTOFLAG)
-RANLIB=$(XC_PREFIX)ranlib $(LTOFLAG)
+define f_gcc_tc_cflags_append =
+    $(call f_util_append_to_symbol,v_gcc_tc_cflags_$1,$2)
+endef
 
 # Linker configuration
 LIBFLAGS=$(MODFLAGS) $(PLATFORM_LIBFLAGS)
@@ -41,7 +76,8 @@ LDFLAGS= -L $(DISTDIR) $(LIBFLAGS)
 
 # C Compiler configuration
 C_INCLUDES+=-Iinclude -I$(CONF_DIR) $(PLATFORM_INCLUDES)
-CFLAGS+=-c $(C_INCLUDES) 
+v_gcc_include_flags=$(v_c_binary_include_path:%=-I%)
+v_gcc_cflags_default_value=-c $(v_gcc_include_flags) 
 CXXFLAGS=$(CFLAGS)
 
 endif
